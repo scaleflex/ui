ff7b8d54b39bfc9736b6f31df8a0d4e8a3abd5be
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2020-12-02 10:09:51 +0200

    chore: create icon Arrow

---
+++ packages/icons/src/arrow.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { intrinsicComponent } from '@sfx-ui/utils/functions';
+import type { IconProps } from './icon.props';
+
+export const Arrow = intrinsicComponent<IconProps, SVGSVGElement>((
+  {
+    // color = 'currentColor',
+    stroke = '#5D6D7E', size = 10, ...rest
+  },
+  ref
+): JSX.Element => (
+  <svg
+    ref={ref}
+    width={size * 0.6}
+    height={size}
+    viewBox="0 0 6 10"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...rest}
+  >
+    <path d="M1 1L5 4.85185L1 9" stroke={stroke} strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+));
+
+export default Arrow;
