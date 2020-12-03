2d0002aefee172f876cdf7ee5c3fc20dfef8d977
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2020-12-03 09:35:21 +0200

    refactor: input-group styles

--- packages/core/src/input-group/input-group.mixin.ts
+++ packages/core/src/input-group/input-group.mixin.ts
@@ -1,19 +1,19 @@
-import { css } from 'styled-components';
-import type { WithTheme } from '@sfx-ui/theme/entity';
-import { Color as PaletteColor } from '@sfx-ui/utils/types/palette';
-// import { Type } from './types';
+// import { css } from 'styled-components';
+// import type { WithTheme } from '@sfx-ui/theme/entity';
+// import { Color as PaletteColor } from '@sfx-ui/utils/types/palette';
+// // import { Type } from './types';
 
-// export const sizeInputGroupMixin = {
-//   [Size.Sm]: css`
-//     height: 24px;
-//   `,
+// // export const sizeInputGroupMixin = {
+// //   [Size.Sm]: css`
+// //     height: 24px;
+// //   `,
 
-//   [Size.Md]: css`
-//     height: 30px;
-//   `,
-// };
+// //   [Size.Md]: css`
+// //     height: 30px;
+// //   `,
+// // };
 
-export const errorMixin = ({ theme: { palette } }: WithTheme) => css`
-  background: ${palette[PaletteColor.BackgroundSecondary]} !important;
-  border: 1px solid ${palette[PaletteColor.Error]} !important;
-`;
+// export const errorMixin = ({ theme: { palette } }: WithTheme) => css`
+//   background: ${palette[PaletteColor.BackgroundSecondary]} !important;
+//   border: 1px solid ${palette[PaletteColor.Error]} !important;
+// `;
