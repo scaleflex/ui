3d76a201f6ec01c6b0c802f0cd3ff380d2d4d377
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2020-12-14 14:55:38 +0200

    fix: loader cloud icon

--- packages/core/src/loader/loader.component.tsx
+++ packages/core/src/loader/loader.component.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { intrinsicComponent } from '@sfx-ui/utils/functions';
-import CloudIcon from '@sfx-ui/icons/cloud';
+import CloudWithShadow from '@sfx-ui/icons/cloud-with-shadow';
 import SpinnerIcon from '@sfx-ui/icons/spinner';
 import type { LoaderProps } from './loader.props';
 import Styled from './loader.styles';
@@ -10,7 +10,7 @@ const Loader = intrinsicComponent<LoaderProps, HTMLSpanElement>((
   ref
 ): JSX.Element => (
   <Styled.Loader {...rest} ref={ref}>
-    <CloudIcon />
+    <CloudWithShadow size={83} />
 
     <Styled.Spinner>
       <SpinnerIcon />
