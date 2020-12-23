5046ab03391e5315302e20917bb7ec64bf9ffac7
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2020-12-23 18:02:19 +0200

    style: in InputGroup/SelectGroup fix margins for FormHint and Label

--- packages/core/src/input-group/input-group.styles.ts
+++ packages/core/src/input-group/input-group.styles.ts
@@ -1,11 +1,21 @@
 import styled from 'styled-components';
 import { generateClassNames } from '@sfx-ui/utils/functions';
+import StyledLabel from '../label/label.styles';
+import StyledFormHint from '../form-hint/form-hint.styles';
 
 const baseClassName = 'InputGroup';
 
 const InputGroup = styled.div.attrs({
   className: generateClassNames(baseClassName, 'root'),
-})``;
+})`
+  ${StyledFormHint.FormHint} {
+    margin-top: 4px;
+  }
+
+  ${StyledLabel.Label} {
+    margin-bottom: 4px;
+  }
+`;
 
 const Styled = { InputGroup };
 
--- packages/core/src/select-group/select-group.styles.ts
+++ packages/core/src/select-group/select-group.styles.ts
@@ -4,10 +4,11 @@ import { generateClassNames } from '@sfx-ui/utils/functions';
 // import type { WithTheme } from '@sfx-ui/theme/entity';
 // import { Color as PColor } from '@sfx-ui/utils/types/palette';
 // import type { SelectGroupProps } from './select-group.props';
+import StyledInputGroup from '../input-group/input-group.styles';
 
 const baseClassName = 'SelectGroup';
 
-const SelectGroup = styled.div.attrs({
+const SelectGroup = styled(StyledInputGroup.InputGroup).attrs({
   className: generateClassNames(baseClassName, 'root'),
 })``;
 
