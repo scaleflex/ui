5a8222177732e4379b52bec4d9d0f6d40f274080
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2021-01-20 13:40:09 +0200

    chore(Tabs): in Tabs story add tabs icons

--- packages/ui/stories/core/tabs.story.tsx
+++ packages/ui/stories/core/tabs.story.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import type { Meta, Story } from '@storybook/react';
+import AssetsIcon from '@scaleflex/icons/assets';
+import CollectionsIcon from '@scaleflex/icons/collections';
+import ProductsIcon from '@scaleflex/icons/products';
 import _Tabs, { TabsProps } from '../../src/core/tabs';
 import Tab from '../../src/core/tab';
 import TabPanel from '../../src/core/tab-panel';
@@ -26,6 +29,7 @@ const BasicTemplate: Story<TabsProps> = ({
   ...args
 }) => {
   const tabs = ['Assets', 'Collections', 'Products'];
+  const tabsIcons = [<AssetsIcon />, <CollectionsIcon />, <ProductsIcon />];
   const [activeTabIndex, setActiveTabIndex] = useState(0);
   const handleTabClick = (newValue: any): void => setActiveTabIndex(newValue);
 
@@ -37,6 +41,7 @@ const BasicTemplate: Story<TabsProps> = ({
             key={tab}
             value={index}
             label={tab}
+            icon={tabsIcons[index]}
           />
         ))}
       </Tabs>
