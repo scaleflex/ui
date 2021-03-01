45197271ba17180a0d55a11285c72f1b51857892
amrelbialy <amrhamdy@outlook.com>
2021-03-01 13:51:28 +0200

    fix: loading in button

--- packages/ui/src/core/button/button.component.tsx
+++ packages/ui/src/core/button/button.component.tsx
@@ -22,7 +22,7 @@ const Button = intrinsicComponent<ButtonProps, HTMLButtonElement>((
     ref={ref}
   >
     {icon && (
-      <Styled.Icon loading={loading}>
+      <Styled.Icon loading={Boolean(loading)}>
         {
           typeof icon === 'function'
             ? (loading ? <SpinnerIcon size={getIconSize(size)} /> : icon({ size: getIconSize(size) }))
@@ -32,7 +32,7 @@ const Button = intrinsicComponent<ButtonProps, HTMLButtonElement>((
     )}
 
     {loading && !icon && (
-      <Styled.Icon loading={loading}>
+      <Styled.Icon loading={Boolean(loading)}>
         <SpinnerIcon size={getIconSize(size)} />
       </Styled.Icon>
     )}
