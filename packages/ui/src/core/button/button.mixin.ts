9554d2253c122f8fd7ba27833d324fa23f247aaf
amrelbialy <amrhamdy@outlook.com>
2021-04-05 13:45:37 +0200

    fix(button): sizes

--- packages/ui/src/core/button/button.mixin.ts
+++ packages/ui/src/core/button/button.mixin.ts
@@ -91,19 +91,19 @@ export const colorButtonMixin = {
 
 export const sizeButtonMixin = {
   [Size.Xs]: css`
-    padding: 6px 8px;
+    padding: 4px 8px;
   `,
 
   [Size.Sm]: css`
-    padding: 8px 10px;
+    padding: 6px 10px;
   `,
 
   [Size.Md]: css`
-    padding: 9px 12px;
+    padding: 8px 12px;
   `,
 
   [Size.Lg]: css`
-    padding: 11px 14px;
+    padding: 10px 14px;
   `,
 
   [Size.Xl]: css`
