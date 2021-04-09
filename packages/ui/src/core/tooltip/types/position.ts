497ae4150fa3fa0bbd111eca320619e8a67808c4
amrelbialy <amrhamdy@outlook.com>
2021-04-09 13:58:05 +0200

    refactor(tooltip): add new values to position

--- packages/ui/src/core/tooltip/types/position.ts
+++ packages/ui/src/core/tooltip/types/position.ts
@@ -1,6 +1,14 @@
 export const Position = {
-  Bottom: 'bottom',
-  Left: 'left',
-  Right: 'right',
+  TopStart: 'top-start',
   Top: 'top',
+  TopEnd: 'top-end',
+  BottomStart: 'bottom-start',
+  Bottom: 'bottom',
+  BottomEnd: 'bottom-end',
+  LeftStart: 'left-start',
+  Left: 'left',
+  LeftEnd: 'left-end',
+  RightStart: 'right-start',
+  Right: 'right',
+  RightEnd: 'right-end',
 } as const;
