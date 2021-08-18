97d7e2657f792e67196e6e203852f4d19780dffd
amrelbialy <amrhamdy@outlook.com>
2021-08-18 21:52:36 +0200

    chore: add logo for demo

---
+++ .storybook/ScaleflexTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+import logo from './public/logo/scaleflex-logo.png'
+
+export default create({
+  base: 'light',
+  brandTitle: 'Scaleflex UI',
+  brandUrl: 'https://github.com/scaleflex/ui',
+  brandImage: logo,
+ });
