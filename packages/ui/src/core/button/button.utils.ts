c62f756230654b676c430e60076995cacb246e1b
Nermeen Moustafa <Nermeen@scaleflex.com>
2022-09-21 14:46:48 +0200

    update buttons to the new design - T10725

--- packages/ui/src/core/button/button.utils.ts
+++ packages/ui/src/core/button/button.utils.ts
@@ -4,13 +4,13 @@ import type { ButtonSizeType } from './button.props';
 export const getIconSize = (sizeName?: ButtonSizeType): number => {
   switch (sizeName) {
     case ButtonSize.Lg:
-      return 24;
+      return 18;
 
     case ButtonSize.Md:
       return 16;
 
     case ButtonSize.Sm:
-      return 12;
+      return 14;
 
     default:
       return 16;
