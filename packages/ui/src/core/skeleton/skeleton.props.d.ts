d119e0046307ab96e539efb9454983a7192d40e1
Amr Hamdy <amrhamdy@outlook.com>
2022-10-11 21:58:44 +0200

    feat(Skeleton): add new component

---
+++ packages/ui/src/core/skeleton/skeleton.props.d.ts
@@ -0,0 +1,10 @@
+// import { Values } from '../../utils/types';
+// import { type } from './types';
+
+// export type SkeletonType = Values<typeof type>;
+
+export interface SkeletonProps extends React.HTMLAttributes<HTMLSpanElement> {
+  width?: stringnumber | string;
+  height?: numner | string;
+  // type?: SkeletonType;
+}
