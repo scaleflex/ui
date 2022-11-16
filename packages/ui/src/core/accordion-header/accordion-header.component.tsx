0325fab5b93af71b8c484635e205184590d6af13
Amr Hamdy <amrhamdy@outlook.com>
2022-11-16 23:30:05 +0200

    refactor(Accordion): styles

--- packages/ui/src/core/accordion-header/accordion-header.component.tsx
+++ packages/ui/src/core/accordion-header/accordion-header.component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PT, { Validator } from 'prop-types';
 import type { IconProps } from '@scaleflex/icons/icon.props';
 import { iconPropTypes } from '@scaleflex/icons/icon.prop-types';
+
 import { intrinsicComponent } from '../../utils/functions';
 import ArrowTick from '../arrow-tick';
 import { Position as ArrowTickType } from '../../utils/types';
@@ -17,12 +18,13 @@ const AccordionHeader = intrinsicComponent<AccordionHeaderProps, HTMLDivElement>
       onClick,
       onContextMenu,
       hideIcon,
+      fullWidth,
       iconProps: iconPropsData,
       ...rest
     }: AccordionHeaderProps,
     ref
   ): JSX.Element => (
-    <Styled.AccordionHeader
+    <Styled.Header
       ref={ref}
       onClick={(event) => {
         if (typeof onChange === 'function') {
@@ -33,30 +35,33 @@ const AccordionHeader = intrinsicComponent<AccordionHeaderProps, HTMLDivElement>
           onClick(event);
         }
       }}
+      fullWidth={fullWidth}
       {...rest}
     >
+      <Styled.Label onContextMenu={onContextMenu}>{label}</Styled.Label>
       {!hideIcon && (
         <Styled.Icon onContextMenu={onContextMenu}>
           <ArrowTick
-            type={expanded ? ArrowTickType.Bottom : ArrowTickType.Right}
+            type={expanded ? ArrowTickType.Top : ArrowTickType.Bottom}
             IconProps={{ size: 11, ...iconPropsData }}
           />
         </Styled.Icon>
       )}
-      <Styled.Label onContextMenu={onContextMenu}>{label}</Styled.Label>
-    </Styled.AccordionHeader>
+    </Styled.Header>
   )
 );
 
 AccordionHeader.defaultProps = {
   expanded: false,
   hideIcon: false,
+  fullWidth: false,
 };
 
 AccordionHeader.propTypes = {
   label: PT.node.isRequired,
   expanded: PT.bool,
   hideIcon: PT.bool,
+  fullWidth: PT.bool,
   onChange: PT.func,
   iconProps: PT.exact(iconPropTypes) as Validator<IconProps>,
 };
