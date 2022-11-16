a0635dc97abe1408e0117ebf5859857275e95e8e
Nermeen Moustafa <Nermeen@scaleflex.com>
2022-11-16 20:52:23 +0200

    feat: update menu item gap - T11614

--- packages/ui/src/core/menu-item/menu-item.styles.ts
+++ packages/ui/src/core/menu-item/menu-item.styles.ts
@@ -69,7 +69,7 @@ const MenuItem = styled.div.attrs({
   }: With<WithTheme, MenuItemProps>) => css`
     display: flex;
     justify-content: space-between;
-    column-gap: 10px;
+    column-gap: 16px;
     flex-direction: row;
     align-items: center;
     width: 100%;
