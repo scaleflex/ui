ed370de0a441f0a278104813164c0146319d4e1f
Nermeen Moustafa <Nermeen@scaleflex.com>
2022-11-20 17:09:51 +0200

    feat: add new colors - T11647

--- packages/ui/src/utils/types/palette/color.ts
+++ packages/ui/src/utils/types/palette/color.ts
@@ -115,6 +115,8 @@ export const Color = {
   FreeImages: 'free-images',
   FreeIcons: 'free-icons',
   Canvas: 'canvas',
+  Box: 'box',
+  ScreenCast: 'screen-cast',
 
   LightShadow: 'light-shadow',
   MediumShadow: 'medium-shadow',
