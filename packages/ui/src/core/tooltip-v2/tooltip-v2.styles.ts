ef3581304776d6f433b8fdcaf016ce9176248cd2
Ahmed Hussien <ahmed.hussien@scaleflex.com>
2024-02-19 12:35:48 +0200

    fix: tooltipV2 overflow bug [FRA-4422]

--- packages/ui/src/core/tooltip-v2/tooltip-v2.styles.ts
+++ packages/ui/src/core/tooltip-v2/tooltip-v2.styles.ts
@@ -24,6 +24,8 @@ const TooltipV2 = styled(Tippy).attrs({
     align-items: center;
     min-width: 54px;
     min-height: 20px;
+    overflow-x: hidden;
+    overflow-y: hidden;
     border-radius: ${theme.shape.borderRadius[BRSize.Md]};
     background: ${$warning ? theme.palette[PColor.BackgroundOrange] : theme.palette[PColor.IconsPlaceholder]};
     color: ${$warning ? theme.palette[PColor.TextWarning] : theme.palette[PColor.LinkActive]};
