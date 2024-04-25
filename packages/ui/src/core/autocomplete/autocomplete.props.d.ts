70b34a67f30b0f1af3c0ab36f590faa528411641
Vitaly Shaposhnik <vitaly@scaleflex.com>
2024-04-25 10:47:40 +0200

    chore: rebuild Autocomplete component

--- packages/ui/src/core/autocomplete/autocomplete.props.d.ts
+++ packages/ui/src/core/autocomplete/autocomplete.props.d.ts
@@ -3,22 +3,25 @@ import type { LabelProps } from '../label';
 import type { MenuProps } from '../menu';
 import type { InputProps } from '../input';
 import { InputBackgroundColor, InputSize } from '../../utils/types';
+import { AnchorElType } from '../menu/menu.props';
 
 export type AutocompleteBackgroundType = Values<typeof InputBackgroundColor>;
 export type AutocompleteSizeType = Values<typeof InputSize>;
-export type AutocompleteOptionType = string | AutocompleteObjectOptionstype;
-export type AutocompleteObjectOptionstype = {
-  id: number;
-  label: string;
+export type AutocompleteOptionIdType = string;
+export type AutocompleteOptionLabelType = string;
+export type AutocompleteOptionObjectType = {
+  [key: string]: any;
 };
+export type AutocompleteOptionType = AutocompleteOptionIdType | AutocompleteOptionObjectType;
+export type AutocompleteValueType = AutocompleteOptionIdType | AutocompleteOptionIdType[];
 
 export interface AutocompleteProps extends React.HTMLAttributes<HTMLDivElement> {
   children?: React.ReactElementLike;
   size?: AutocompleteSizeType;
   label?: React.ReactNode;
   hint?: React.ReactNode;
-  value?: any;
-  options: string[] | AutocompleteObjectOptionstype[];
+  value?: AutocompleteValueType;
+  options: AutocompleteOptionType[];
   noOptionsText?: string;
   placeholder?: string;
   fullWidth?: boolean;
@@ -29,17 +32,62 @@ export interface AutocompleteProps extends React.HTMLAttributes<HTMLDivElement> 
   error?: boolean;
   sortAlphabetically?: boolean;
   focusOnOpen?: boolean;
-  onChange?: (event: React.SyntheticEvent<HTMLInputElement>, value: string | AutocompleteValueType[]) => void;
-  onOpen?: (event) => void;
-  onClose?: (event: React.SyntheticEvent<HTMLInputElement> | React.KeyboardEvent<HTMLInputElement>) => void;
-  getOptionDisabled?: (item: string, index: number) => boolean;
-  getOptionValue?: (
-    value: string | AutocompleteObjectOptionstype | any
-  ) => string | AutocompleteObjectOptionstype[] | any;
-  getOptionLabel?: (value: string | AutocompleteObjectOptionstype | any) => string | any;
+  onChange?: (newValue: AutocompleteValueType) => void;
+  onOpen?: (event: React.SyntheticEvent<HTMLElement>) => void;
+  onClose?: (event: React.SyntheticEvent<HTMLElement> | React.KeyboardEvent<HTMLElement> | undefined) => void;
+  getOptionDisabled?: (option: AutocompleteOptionType) => boolean;
+  getOptionValue?: (option: AutocompleteOptionType) => AutocompleteOptionIdType;
+  getOptionLabel?: (option: AutocompleteOptionType) => AutocompleteOptionLabelType;
   submitOnBlur?: () => void;
   MenuProps?: MenuProps;
   LabelProps?: LabelProps;
   InputProps?: InputProps;
   background?: AutocompleteBackgroundType;
+  maxMenuHeight?: string | number;
+  showClearIcon?: boolean;
+  renderLabelIconEnd?: ({
+    option,
+    isMultiple,
+  }: {
+    option?: AutocompleteOptionType | null | undefined;
+    isMultiple: boolean;
+  }) => React.ReactNode;
+  renderOptionLabel?: (option: AutocompleteOptionType) => React.ReactNode;
+  renderSearchEmptyMenuItem?: () => React.ReactNode;
+}
+
+export interface RenderLabelProps {
+  label?: React.ReactNode;
+  error?: boolean;
+  size?: AutocompleteSizeType;
+  LabelProps?: LabelProps;
+}
+
+export interface RenderHintProps {
+  hint?: React.ReactNode;
+  error?: boolean;
+  size?: AutocompleteSizeType;
+}
+
+export interface AutocompleteHookReturn {
+  formattedValue: AutocompleteValueType;
+  filteredOptions: AutocompleteOptionType[];
+  optionsList: AutocompleteOptionType[];
+  inputRef: React.Ref;
+  inputValue: string;
+  isValueSelected?: boolean;
+  open: boolean;
+  anchorEl: AnchorElType;
+  handleMenuItemClick: (option: AutocompleteOptionType) => () => void;
+  handleOpenMenuClick: (event: React.SyntheticEvent<HTMLSpanElement>) => void;
+  handleChangeSearchTerm: (event: React.SyntheticEvent<HTMLInputElement>) => void;
+  handleCloseMenuClick: (
+    event?: React.SyntheticEvent<HTMLInputElement> | React.KeyboardEvent<HTMLInputElement>
+  ) => void;
+  checkIsIdSelected: (optionId: AutocompleteOptionIdType) => boolean;
+  getOptionById: (optionId: AutocompleteOptionIdType) => AutocompleteOptionType | undefined | null;
+  handleOnRemoveItem: (event: React.SyntheticEvent<HTMLElement>, optionId: AutocompleteOptionIdType) => void;
+  handleOnBlur: () => void;
+  handleKeyDown: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+  handleClearIconClick: () => void;
 }
--- packages/ui/src/core/autocomplete/autocomplete.styles.ts
+++ packages/ui/src/core/autocomplete/autocomplete.styles.ts
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components';
 import CrossOutline from '@scaleflex/icons/cross-outline';
 import StyledLabel from '../label/label.styles';
 import StyledFormHint from '../form-hint/form-hint.styles';
+import SfxMenu from '../menu';
 import { generateClassNames, applyDisplayNames } from '../../utils/functions';
 import type { WithTheme } from '../../theme/entity';
 import { Color as PColor } from '../../utils/types/palette';
@@ -50,6 +51,28 @@ const AutocompleteContainer = styled.div.attrs({
   className: generateClassNames(baseClassName, 'container'),
 })``;
 
-const Styled = applyDisplayNames({ Autocomplete, AutocompleteContainer, TickIcon, CrossIcon });
+const InputIconEndContainer = styled.div.attrs({
+  className: generateClassNames(baseClassName, 'inputIconEndContainer'),
+})`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+`;
+
+const Menu = styled(SfxMenu).attrs({
+  className: generateClassNames(baseClassName, 'menu'),
+})`
+  max-height: ${({ maxMenuHeight }: { maxMenuHeight: string | number }) => (+maxMenuHeight ? `${+maxMenuHeight}px` : maxMenuHeight)};
+  background: #ffffff;
+`;
+
+const Styled = applyDisplayNames({
+  Autocomplete,
+  AutocompleteContainer,
+  TickIcon,
+  CrossIcon,
+  InputIconEndContainer,
+  Menu,
+});
 
 export default Styled;
