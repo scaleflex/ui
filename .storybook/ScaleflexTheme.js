251594990af712b8256e23176e81e449dd4df528
Amr Hamdy <amrelbialy26@gmail.com>
2024-05-15 20:32:06 +0300

    demo: add new scaleflex logo

--- .storybook/ScaleflexTheme.js
+++ .storybook/ScaleflexTheme.js
@@ -1,5 +1,5 @@
 import { create } from '@storybook/theming';
-import logo from './public/logo/scaleflex-logo.png'
+import logo from './public/logo/scaleflex-logo-white.png'
 
 export default create({
   brandTitle: 'Scaleflex UI',
