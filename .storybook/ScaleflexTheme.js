fa7b888f303c94086c4b6ff49ecc22a925960349
Amr Hamdy <amrelbialy26@gmail.com>
2024-05-22 18:30:23 +0300

    add default theme for storybook

--- .storybook/ScaleflexTheme.js
+++ .storybook/ScaleflexTheme.js
@@ -2,6 +2,7 @@ import { create } from '@storybook/theming';
 import logo from './public/logo/scaleflex-logo-white.png'
 
 export default create({
+  base: 'dark',
   brandTitle: 'Scaleflex UI',
   brandImage: logo,
  });
