6dde5079f21337f23a180ed1f25092120d7cc15c
Ahmed Hussien <ahmed.hussien@scaleflex.com>
2024-05-24 15:37:56 +0300

    add accent opacity055 color

--- packages/ui/src/utils/types/palette/color.ts
+++ packages/ui/src/utils/types/palette/color.ts
@@ -14,6 +14,7 @@ export const Color = {
   AccentSecondaryDisabled: 'accent-secondary-disabled',
   AccentStateless: 'accent-stateless',
   AccentStateless_0_4_Opacity: 'accent-stateless_0_4_opacity',
+  Accent_0_5_5_Opacity: 'accent_0_5_5_opacity',
   Accent_0_5_Opacity: 'accent_0_5_opacity',
   Accent_1_2_Opacity: 'accent_1_2_opacity',
   Accent_1_8_Opacity: 'accent_1_8_opacity',
