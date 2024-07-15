ce8339572d5a84474118693cf98d3162d9fd19a7
Amr Hamdy <amrelbialy26@gmail.com>
2024-07-15 12:18:17 +0300

    Docs(Button): add docs page for button

--- packages/ui/src/core/button/button.styles.ts
+++ packages/ui/src/core/button/button.styles.ts
@@ -40,7 +40,7 @@ const Wrapper = styled.span.attrs({
 `;
 const Button = styled.button.attrs<With<WithTheme, ButtonProps>>({
   className: generateClassNames(baseClassName, 'root'),
-})<With<With<WithTheme, ButtonProps>, { $buttonType: ButtonType }>>(
+})<With<With<WithTheme, ButtonProps>, { $buttonType?: ButtonType }>>(
   ({ color = ButtonColor.Secondary, size = ButtonSize.Md, $buttonType, active, theme }) => css`
     display: inline-flex;
     flex-shrink: 0;
