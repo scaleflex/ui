34806c700a8e430f2e43d1860326e7eda2c98baa
Ahmed Hussien <ahmed.hussien@scaleflex.com>
2024-10-11 12:09:06 +0300

    fix: duplicate icon is not aligned [FRA-6734]

--- packages/icons/src/duplicate.tsx
+++ packages/icons/src/duplicate.tsx
@@ -8,7 +8,7 @@ export const Duplicate = intrinsicComponent<IconProps, SVGSVGElement>(
       ref={ref}
       width={size}
       height={size}
-      viewBox="0 0 50 50"
+      viewBox="-6 -3 50 50"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...rest}
